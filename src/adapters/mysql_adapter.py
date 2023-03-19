abb7daea4394a02df7056134b4eb79a84301486d
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2023-03-19 15:02:03 +0100

    add used tokens

--- src/adapters/mysql_adapter.py
+++ src/adapters/mysql_adapter.py
@@ -18,7 +18,7 @@ def sql_setup(host, port, user, password, database):
 def insert_message(chat_id, username, datetime, message_id, text, reply_message_id):
     global cnx
     cursor = cnx.cursor()
-    sql = "INSERT INTO MESSAGES (CHAT_ID, USERNAME, DATETIME, MESSAGE_ID, TEXT, REPLY_MESSAGE_ID) VALUES ( %s, %s, %s, %s, %s, %s)"
+    sql = 'INSERT INTO MESSAGES (CHAT_ID, USERNAME, DATETIME, MESSAGE_ID, TEXT, REPLY_MESSAGE_ID) VALUES (%s, %s, %s, %s, %s, %s)'
     val = (chat_id, username, datetime, message_id, text, reply_message_id)
     cursor.execute(sql, val)
     cnx.commit()
@@ -27,14 +27,21 @@ def insert_message(chat_id, username, datetime, message_id, text, reply_message_
 def does_user_exist(username):
     global cnx
     cursor = cnx.cursor()
-    sql = "SELECT USERNAME FROM USERS WHERE USERNAME = %s"
+    sql = 'SELECT USERNAME FROM USERS WHERE USERNAME = %s'
     val = (username,)
     cursor.execute(sql, val)
     result = cursor.fetchone()
     cnx.commit()
-    print(val, result)
-
     if result is not None:
         return True
     else:
         return False
+
+
+def update_message_tokens(message_id, completion_tokens, prompt_tokens):
+    global cnx
+    cursor = cnx.cursor()
+    sql = 'UPDATE MESSAGES SET COMPLETION_TOKENS=%s, PROMPT_TOKENS=%s WHERE MESSAGE_ID = %s'
+    val = (completion_tokens, prompt_tokens, message_id)
+    cursor.execute(sql, val)
+    cnx.commit()
--- src/adapters/openai_adapter.py
+++ src/adapters/openai_adapter.py
@@ -12,9 +12,13 @@ def setup(api_key):
 def generate_response(prompt):
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=[{"role": "user", "content": prompt}],
+        messages=[{'role': 'user', 'content': prompt}],
         max_tokens=1024,
         n=1,
         temperature=1,
     )
-    return response.choices[0].message.content
+    content = response.choices[0].message.content
+    completion_tokens = response.usage.completion_tokens
+    prompt_tokens = response.usage.prompt_tokens
+
+    return {'content': content, 'tokens': {'completion_tokens': completion_tokens, 'prompt_tokens': prompt_tokens}}
