b3c613bf2ee9ec0bd0b6a7844680d9ed0e2e657d
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2023-03-23 13:02:18 +0100

    fixed source env problem

--- src/main.py
+++ src/main.py
@@ -2,20 +2,19 @@ from bot import run
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 
 def get_config(config_name):
     return os.environ.get(config_name)
 
 
 if __name__ == '__main__':
+    load_dotenv('src/config/.env')
     BOT_TOKEN = get_config('BOT_TOKEN')
     OPENAI_API_KEY = get_config('OPENAI_API_KEY')
     HOST = get_config('HOST')
     PORT = get_config('PORT')
-    USER = get_config('USER')
+    DB_USER = get_config('DB_USER')
     PASSWORD = get_config('PASSWORD')
     DATABASE = get_config('DATABASE')
 
-    run(BOT_TOKEN, OPENAI_API_KEY, HOST, PORT, USER, PASSWORD, DATABASE)
+    run(BOT_TOKEN, OPENAI_API_KEY, HOST, PORT, DB_USER, PASSWORD, DATABASE)
