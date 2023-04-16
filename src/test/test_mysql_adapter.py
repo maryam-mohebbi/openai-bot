a2a9faea94f6b3d95ed97d5b7b572c34f9779ac2
Maryam Mohebbi <88187675+maryam-mohebbi@users.noreply.github.com>
2023-04-16 14:53:43 +0200

    add connection pool to MySQL adapter (#18)
    * add connection pool to MySQL adapther
    
    * add connection pool to MySQL adapther
    
    * fix tests

--- src/test/test_mysql_adapter.py
+++ src/test/test_mysql_adapter.py
@@ -1,57 +1,66 @@
 import unittest
 from unittest.mock import MagicMock, patch, call
+from mysql.connector import pooling
 from adapters import mysql_adapter as mysql
 
 
 class MySqlAdapterTest_sql_setup(unittest.TestCase):
-    @patch('mysql.connector.connect')
-    def test_should_verify_global_variable_is_correctly_set_based_on_input(self, mock_connect):
+    @patch('mysql.connector.pooling.MySQLConnectionPool')
+    def test_should_verify_global_variable_is_correctly_set_based_on_input(self, mock_connection_pool):
         # prepare
-        mock_connect.return_value = MagicMock()
+        mock_connection_pool.return_value = MagicMock()
 
         # run
         result = mysql.sql_setup('test_host', 'test_port',
                                  'test_user', 'test_password', 'test_database')
 
         # assert
-        mock_connect.assert_called_with(
-            host='test_host',
-            port='test_port',
-            user='test_user',
-            password='test_password',
-            database='test_database'
-        )
-        self.assertEqual(mysql.cnx,
-                         mock_connect.return_value)
+        mock_connection_pool.assert_called_with(pool_name="pynative_pool",
+                                                pool_size=20,
+                                                pool_reset_session=True,
+                                                host='test_host',
+                                                port='test_port',
+                                                user='test_user',
+                                                password='test_password',
+                                                database='test_database')
+        self.assertEqual(mysql.connection_pool,
+                         mock_connection_pool.return_value)
 
 
 class MySqlAdapterTest_insert_message(unittest.TestCase):
-    @patch.object(mysql, 'cnx')
-    def test_should_insert_message_correctly(self, mock_cnx):
+    @patch.object(mysql, 'connection_pool')
+    def test_should_insert_message_correctly(self, mock_connection_pool):
         # prepare
+        mock_connection_object = MagicMock()
         mock_cursor = MagicMock()
-        mock_cnx.cursor.return_value = mock_cursor
+        mock_connection_pool.get_connection.return_value = mock_connection_object
+        mock_connection_object.cursor.return_value = mock_cursor
 
         # run
         mysql.insert_message('chat_id', 'username', 'datetime',
                              'message_id', 'text', 'reply_message_id')
 
         # assert
-        mock_cnx.cursor.assert_called_once()
+        mock_connection_pool.get_connection.assert_called_once()
+        mock_connection_object.cursor.assert_called_once()
         mock_cursor.execute.assert_called_with(
             'INSERT INTO MESSAGES (CHAT_ID, USERNAME, DATETIME, MESSAGE_ID, TEXT, REPLY_MESSAGE_ID) VALUES (%s, %s, %s, %s, %s, %s)',
             ('chat_id', 'username', 'datetime',
              'message_id', 'text', 'reply_message_id')
         )
-        mock_cnx.commit.assert_called_once()
+        mock_connection_object.commit.assert_called_once()
+        mock_cursor.close.assert_called_once()
+        mock_connection_object.close.assert_called_once()
 
 
 class MySqlAdapterTest_does_user_exist(unittest.TestCase):
-    @patch.object(mysql, 'cnx')
-    def test_should_return_true_when_user_exists(self, mock_cnx):
+    @patch.object(mysql, 'connection_pool')
+    def test_should_return_true_when_user_exists(self, mock_connection_pool):
         # prepare
+        mock_connection_object = MagicMock()
         mock_cursor = MagicMock()
-        mock_cnx.cursor.return_value = mock_cursor
+        mock_connection_pool.get_connection.return_value = mock_connection_object
+        mock_connection_object.cursor.return_value = mock_cursor
         mock_cursor.fetchone.return_value = ('test_user',)
 
         # run
@@ -63,13 +72,16 @@ class MySqlAdapterTest_does_user_exist(unittest.TestCase):
             'SELECT USERNAME FROM USERS WHERE USERNAME = %s',
             ('test_user',)
         )
-        mock_cnx.commit.assert_called_once()
+        mock_connection_pool.get_connection.assert_called_once()
+        mock_connection_object.cursor.assert_called_once()
 
-    @patch.object(mysql, 'cnx')
-    def test_should_return_false_when_user_does_not_exist(self, mock_cnx):
+    @patch.object(mysql, 'connection_pool')
+    def test_should_return_false_when_user_does_not_exist(self, mock_connection_pool):
         # prepare
+        mock_connection_object = MagicMock()
         mock_cursor = MagicMock()
-        mock_cnx.cursor.return_value = mock_cursor
+        mock_connection_pool.get_connection.return_value = mock_connection_object
+        mock_connection_object.cursor.return_value = mock_cursor
         mock_cursor.fetchone.return_value = None
 
         # run
@@ -81,35 +93,41 @@ class MySqlAdapterTest_does_user_exist(unittest.TestCase):
             'SELECT USERNAME FROM USERS WHERE USERNAME = %s',
             ('test_user',)
         )
-        mock_cnx.commit.assert_called_once()
+        mock_connection_pool.get_connection.assert_called_once()
+        mock_connection_object.cursor.assert_called_once()
 
 
 class MySqlAdapterTest_update_message_tokens(unittest.TestCase):
-    @patch.object(mysql, 'cnx')
-    def test_should_update_message_tokens_correctly(self, mock_cnx):
+    @patch.object(mysql, 'connection_pool')
+    def test_should_update_message_tokens_correctly(self, mock_connection_pool):
         # prepare
+        mock_connection_object = MagicMock()
         mock_cursor = MagicMock()
-        mock_cnx.cursor.return_value = mock_cursor
-
+        mock_connection_pool.get_connection.return_value = mock_connection_object
+        mock_connection_object.cursor.return_value = mock_cursor
         # run
         mysql.update_message_tokens(
             'message_id', 'completion_tokens', 'prompt_tokens')
 
         # assert
-        mock_cnx.cursor.assert_called_once()
+        mock_connection_pool.get_connection.assert_called_once()
+        mock_connection_object.cursor.assert_called_once()
         mock_cursor.execute.assert_called_with(
             'UPDATE MESSAGES SET COMPLETION_TOKENS=%s, PROMPT_TOKENS=%s WHERE MESSAGE_ID = %s',
             ('completion_tokens', 'prompt_tokens', 'message_id')
         )
-        mock_cnx.commit.assert_called_once()
+        mock_connection_pool.get_connection.assert_called_once()
+        mock_connection_object.cursor.assert_called_once()
 
 
 class MySqlAdapterTest_token_count(unittest.TestCase):
-    @patch.object(mysql, 'cnx')
-    def test_tokens_count(self, mock_cnx):
+    @patch.object(mysql, 'connection_pool')
+    def test_tokens_count(self, mock_connection_pool):
         # Mock the cursor and its methods
+        mock_connection_object = MagicMock()
         mock_cursor = MagicMock()
-        mock_cnx.cursor.return_value = mock_cursor
+        mock_connection_pool.get_connection.return_value = mock_connection_object
+        mock_connection_object.cursor.return_value = mock_cursor
         mock_cursor.fetchone.side_effect = [
             (0,),  # user_count
             (10, 20),  # result_token
